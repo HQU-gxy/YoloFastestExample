a6eaab41d46edef8a5f672dd7001073809d4c073
crosstyan <crosstyan@outlook.com>
2022-02-08 01:40:35 +0800

    chore: reformat

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,57 +1,56 @@
 #include "yolo-fastestv2.h"
 
-int main()
-{   
-    static const char* class_names[] = {
-        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
-        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
-        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
-        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
-        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
-        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
-        "hair drier", "toothbrush"
-    };
-    
-    yoloFastestv2 api;
+int main() {
+  static const char *class_names[] = {
+      "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+      "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+      "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+      "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+      "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+      "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+      "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+      "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+      "hair drier", "toothbrush"
+  };
 
-    api.loadModel("./model/yolo-fastestv2-opt.param",
-                  "./model/yolo-fastestv2-opt.bin");
+  yoloFastestv2 api;
 
-    cv::Mat cvImg = cv::imread("test.jpg");
+  api.loadModel("./model/yolo-fastestv2-opt.param",
+                "./model/yolo-fastestv2-opt.bin");
 
-    std::vector<TargetBox> boxes;
-    api.detection(cvImg, boxes);
+  cv::Mat cvImg = cv::imread("test.jpg");
 
-    for (int i = 0; i < boxes.size(); i++) {
-        std::cout<<boxes[i].x1<<" "<<boxes[i].y1<<" "<<boxes[i].x2<<" "<<boxes[i].y2
-                 <<" "<<boxes[i].score<<" "<<boxes[i].cate<<std::endl;
-        
-        char text[256];
-        sprintf(text, "%s %.1f%%", class_names[boxes[i].cate], boxes[i].score * 100);
+  std::vector<TargetBox> boxes;
+  api.detection(cvImg, boxes);
 
-        int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+  for (int i = 0; i < boxes.size(); i++) {
+    std::cout << boxes[i].x1 << " " << boxes[i].y1 << " " << boxes[i].x2 << " " << boxes[i].y2
+              << " " << boxes[i].score << " " << boxes[i].cate << std::endl;
 
-        int x = boxes[i].x1;
-        int y = boxes[i].y1 - label_size.height - baseLine;
-        if (y < 0)
-            y = 0;
-        if (x + label_size.width > cvImg.cols)
-            x = cvImg.cols - label_size.width;
+    char text[256];
+    sprintf(text, "%s %.1f%%", class_names[boxes[i].cate], boxes[i].score * 100);
 
-        cv::rectangle(cvImg, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-                      cv::Scalar(255, 255, 255), -1);
+    int baseLine = 0;
+    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
 
-        cv::putText(cvImg, text, cv::Point(x, y + label_size.height),
-                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+    int x = boxes[i].x1;
+    int y = boxes[i].y1 - label_size.height - baseLine;
+    if (y < 0)
+      y = 0;
+    if (x + label_size.width > cvImg.cols)
+      x = cvImg.cols - label_size.width;
 
-        cv::rectangle (cvImg, cv::Point(boxes[i].x1, boxes[i].y1), 
-                       cv::Point(boxes[i].x2, boxes[i].y2), cv::Scalar(255, 255, 0), 2, 2, 0);
-    }
-    
-    cv::imwrite("output.png", cvImg);
+    cv::rectangle(cvImg, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                  cv::Scalar(255, 255, 255), -1);
 
-    return 0;
+    cv::putText(cvImg, text, cv::Point(x, y + label_size.height),
+                cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+
+    cv::rectangle(cvImg, cv::Point(boxes[i].x1, boxes[i].y1),
+                  cv::Point(boxes[i].x2, boxes[i].y2), cv::Scalar(255, 255, 0), 2, 2, 0);
+  }
+
+  cv::imwrite("output.png", cvImg);
+
+  return 0;
 }
