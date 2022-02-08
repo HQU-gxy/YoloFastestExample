d7ffe18bb56b4f7d0818c6be9dde1cbacece5348
crosstyan <crosstyan@outlook.com>
2022-02-08 14:26:35 +0800

    feat: time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,24 @@
+#include <benchmark.h>
 #include "yolo-fastestv2.h"
 #include "CLI/App.hpp"
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
 
-int main(int argc, char** argv) {
-  static const char *class_names[] = {
-      "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
-      "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-      "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
-      "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-      "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
-      "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
-      "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
-      "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
-      "hair drier", "toothbrush"
-  };
-
-  // ./Yolo-Fastestv2 -i ../test.jpg -p ../model/yolo-fastestv2-opt.param -b ../model/yolo-fastestv2-opt.bin
-  CLI::App app{"A example YOLO Fastest v2 application"};
-  std::string inputFileName = "";
-  std::string outputFilename = "output.png";
-  std::string paramPath = "";
-  std::string binPath = "";
-  app.add_option("-i,--input", inputFileName, "Input file location, with extension name of jpg")->required()->check(CLI::ExistingFile);
-  app.add_option("-o,--output", outputFilename, "Output file location, with extension name of png");
-  app.add_option("-p,--param", paramPath, "ncnn network parameters file")->required()->check(CLI::ExistingFile);
-  app.add_option("-b,--bin", binPath, "ncnn network bin file")->required()->check(CLI::ExistingFile);
-
-  CLI11_PARSE(app, argc, argv);
-
-  yoloFastestv2 api;
-
-  api.loadModel(paramPath.c_str(), binPath.c_str());
-
-  cv::Mat cvImg = cv::imread(inputFileName);
-
+// not a pure function, will modify the cvImg
+// @param className - the name of the class to be detected (array of strings)
+std::vector<TargetBox> detectFrame(cv::Mat& cvImg, yoloFastestv2& api, const std::vector<char const *> className){
   std::vector<TargetBox> boxes;
+
+  auto start = ncnn::get_current_time();
   api.detection(cvImg, boxes);
+  auto end = ncnn::get_current_time();
+  auto time = end - start;
+  std::cout << "Time: " << time << " ms" << std::endl;
+  cv::putText(cvImg, std::to_string(time) + " ms", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
 
-  for (TargetBox box : boxes) {
-    // print out the boxes
-    std::cout << box.x1 << " " << box.y1 << " " << box.x2 << " " << box.y2
-              << " " << box.score << " " << class_names[box.cate] << std::endl;
-
+  for (TargetBox box: boxes) {
     char text[256];
-    sprintf(text, "%s %.1f%%", class_names[box.cate], box.score * 100);
+    sprintf(text, "%s %.1f%%", className[box.cate], box.score * 100);
 
     int baseLine = 0;
     cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
@@ -65,8 +39,49 @@ int main(int argc, char** argv) {
     cv::rectangle(cvImg, cv::Point(box.x1, box.y1),
                   cv::Point(box.x2, box.y2), cv::Scalar(255, 255, 0), 2, 2, 0);
   }
+  return boxes;
+}
 
+int main(int argc, char **argv) {
+  static std::vector<char const *> class_names = {
+      "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+      "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+      "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+      "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+      "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+      "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+      "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+      "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+      "hair drier", "toothbrush"
+  };
+
+  // ./Yolo-Fastestv2 -i ../test.jpg -p ../model/yolo-fastestv2-opt.param -b ../model/yolo-fastestv2-opt.bin
+  CLI::App app{"A example YOLO Fastest v2 application"};
+  std::string inputFileName = "";
+  std::string outputFilename = "output.png";
+  std::string paramPath = "";
+  std::string binPath = "";
+  app.add_option("-i,--input", inputFileName, "Input file location")->required()->check(
+      CLI::ExistingFile);
+  app.add_option("-o,--output", outputFilename, "Output file location");
+  app.add_option("-p,--param", paramPath, "ncnn network prototype file (end with .param)")->required()->check(CLI::ExistingFile);
+  app.add_option("-b,--bin", binPath, "ncnn network model file (end with .bin)")->required()->check(CLI::ExistingFile);
+
+  CLI11_PARSE(app, argc, argv);
+
+  yoloFastestv2 api;
+  api.loadModel(paramPath.c_str(), binPath.c_str());
+
+  cv::Mat cvImg = cv::imread(inputFileName);
+  auto boxes = detectFrame(cvImg, api, class_names);
+  for (TargetBox box : boxes) {
+    // print out the boxes
+    std::cout << box.x1 << "\t" << box.y1 << "\t" << box.x2 << "\t" << box.y2
+              << "\t" << box.score << "\t" << class_names[box.cate] << std::endl;
+  }
   cv::imwrite(outputFilename, cvImg);
 
   return 0;
 }
+
+
