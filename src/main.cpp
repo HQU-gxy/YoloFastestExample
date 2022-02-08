83f2bc1dea4a7dfc853b27ff2c876f69129a68b5
crosstyan <crosstyan@outlook.com>
2022-02-08 15:21:26 +0800

    chore: format

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #include "spdlog/spdlog.h"
 
 // not a pure function, will modify the cvImg
-// @param className - the name of the class to be detected (array of strings)
-std::vector<TargetBox> detectFrame(cv::Mat& cvImg, yoloFastestv2& api, const std::vector<char const *> className){
+// @param classNames - the name of the class to be detected (array of strings)
+std::vector<TargetBox> detectFrame(cv::Mat &cvImg, yoloFastestv2 &api, const std::vector<char const *> classNames) {
   std::vector<TargetBox> boxes;
 
   auto start = ncnn::get_current_time();
@@ -17,11 +17,12 @@ std::vector<TargetBox> detectFrame(cv::Mat& cvImg, yoloFastestv2& api, const std
   auto end = ncnn::get_current_time();
   auto time = end - start;
   spdlog::info("detection time: {} ms", time);
-  cv::putText(cvImg, std::to_string(time) + " ms", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+  cv::putText(cvImg, std::to_string(time) + " ms", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1,
+              cv::Scalar(0, 0, 255), 2);
 
   for (TargetBox box: boxes) {
     char text[256];
-    sprintf(text, "%s %.1f%%", className[box.cate], box.score * 100);
+    sprintf(text, "%s %.1f%%", classNames[box.cate], box.score * 100);
 
     int baseLine = 0;
     cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
@@ -46,7 +47,7 @@ std::vector<TargetBox> detectFrame(cv::Mat& cvImg, yoloFastestv2& api, const std
 }
 
 int main(int argc, char **argv) {
-  static const std::vector<char const *> class_names = {
+  static const std::vector<char const *> classNames = {
       "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
       "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
       "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
@@ -68,7 +69,8 @@ int main(int argc, char **argv) {
   app.add_option("-i,--input", inputFileName, "Input file location")->required()->check(
       CLI::ExistingFile);
   app.add_option("-o,--output", outputFilename, "Output file location");
-  app.add_option("-p,--param", paramPath, "ncnn network prototype file (end with .param)")->required()->check(CLI::ExistingFile);
+  app.add_option("-p,--param", paramPath, "ncnn network prototype file (end with .param)")->required()->check(
+      CLI::ExistingFile);
   app.add_option("-b,--bin", binPath, "ncnn network model file (end with .bin)")->required()->check(CLI::ExistingFile);
   app.add_flag("-d,--debug", isDebug, "Enable debug log");
 
@@ -81,11 +83,11 @@ int main(int argc, char **argv) {
   api.loadModel(paramPath.c_str(), binPath.c_str());
 
   cv::Mat cvImg = cv::imread(inputFileName);
-  auto boxes = detectFrame(cvImg, api, class_names);
-  if(isDebug) {
+  auto boxes = detectFrame(cvImg, api, classNames);
+  if (isDebug) {
 //    spdlog::debug("{}\t{}\t{}\t{}\t{}\t{}", "x1", "y1", "x2", "y2", "score", "class");
-    for (TargetBox box : boxes) {
-      spdlog::debug("{}\t{}\t{}\t{}\t{}\t{}", box.x1, box.y1, box.x2, box.y2, box.score, class_names[box.cate]);
+    for (TargetBox box: boxes) {
+      spdlog::debug("{}\t{}\t{}\t{}\t{}\t{}", box.x1, box.y1, box.x2, box.y2, box.score, classNames[box.cate]);
     }
   }
   cv::imwrite(outputFilename, cvImg);
