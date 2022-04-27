a6e4fee5b2c88ff4e19cf8889aed7314a0214c53
crosstyan <crosstyan@outlook.com>
2022-04-27 17:05:31 +0800

    works

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ print(so_root)
 sys.path.insert(0, so_root)
 import yolo_app
 
+#  -i 0 -p ../model/yolo-fastestv2-opt.param -b ../model/yolo-fastestv2-opt.bin -d -s 0.2 --nms 0.125 --rtmp rtmp://localhost:1935/live/test
 opts_dict = {
     "input_file_path": "0",
     "output_file_path": "",
@@ -17,8 +18,8 @@ opts_dict = {
     "bin_path": os.path.join(pwd, "model", "yolo-fastestv2-opt.bin"),
     "rtmp_url": "rtmp://localhost:1935/live/test",
     "redis_url": "tcp://127.0.0.1:6379",
-    "scaled_coeffs": 1.0,
-    "threshold_NMS": 0.1,
+    "scaled_coeffs": 0.2,
+    "threshold_NMS": 0.125,
     "out_fps": 5,
     "crop_coeffs": 0.1,
     "threads_num": 4,
@@ -29,6 +30,7 @@ opts = yolo_app.init_options(opts_dict)
 main = yolo_app.MainWrapper(opts)
 main.init()
 main.run_push()
+main.run_pull()
 
 while True:
     time.sleep(5)
